e687de3d5584f0870941238f506133e40ecc83ad
Ramimashkouk <ramima1996@gmail.com>
2022-06-12 10:25:27 +0300

    fix: Allow to update multiple nodes together
    Remove the constraint of kind-existence

--- kg_api/core/graph.py
+++ kg_api/core/graph.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import logging
 import datetime
 from neomodel import db, config, clear_neo4j_database
 from kg_api.utils.settings import OntologySettings
@@ -81,7 +82,9 @@ def update_node(
         change_date = datetime.datetime.now()
 
     nodes = search_nodes(kind, properties_filter)
-    if len(nodes) == 1:  # we need to update exactly one node
+    if nodes:
+        if len(nodes) > 1:
+            logging.info("Updating multiple nodes")
         match_a, properties_filter = querymaker.match_node_query("a", kind, properties_filter)
         with_ = querymaker.with_query(["a"])
         set_query, updated_updates = querymaker.patch_property_query(
@@ -90,6 +93,8 @@ def update_node(
         params = {**properties_filter, **updated_updates}
         query = "\n".join([match_a, with_, set_query])
         db.cypher_query(query, params)
+    else:
+        logging.error("There isn't such a node to be updated")
 
 
 def delete_node(
@@ -195,14 +200,14 @@ def search_relationships(
         filter_a = {}
     if filter_b is None:
         filter_b = {}
+
     match_a, match_b = [""] * 2
-    if kind_a:
-        match_a, filter_a = querymaker.match_node_query("a", kind_a, filter_a)
-    if kind_b:
-        match_b, filter_b = querymaker.match_node_query("b", kind_b, filter_b)
+    match_a, filter_a = querymaker.match_node_query("a", kind_a, filter_a)
+    match_b, filter_b = querymaker.match_node_query("b", kind_b, filter_b)
     rel_query, rel_properties_filter = querymaker.match_relationship_query(
         "a", "r", relationship, rel_properties_filter, "b"
     )
+
     return_ = f"RETURN source, r, destination\nLIMIT {limit}"
 
     query = "\n".join([match_a, match_b, rel_query, return_])
