82c7ee6e365d8a0f89ac8c85e05e0436faef46e2
mtalimanchuk <mtalimanchuk@gmail.com>
2022-07-21 18:02:05 +0300

    add setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+authors = [
+    "Daniel Kornev"
+    "Rami Mahskouk"
+    "Maxim Talimanchuk <mtalimanchuk@gmail.com>",
+]
+
+with open("requirements.txt", "r", encoding="utf-8") as req_f:
+    install_requires = [line.strip() for line in req_f if line.strip()]
+
+setup(
+    name="deeppavlov-kg",
+    author=", ".join(authors),
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+)
