724513b4efe904d9e33cf06a88911c5b4be5a249
Ramimashkouk <ramima1996@gmail.com>
2023-08-16 10:36:23 +0300

    fix: Encode symbols before storing in index

--- deeppavlov_kg/core/index.py
+++ deeppavlov_kg/core/index.py
@@ -28,6 +28,7 @@ class Index:
 
     def add_entities(self, entity_substr_list, entity_ids_list, tags_list):
         assert self.user_id is not None, "Active user_id should be defined. Use set_active_user_id method" #TODO: think about not adding abstract inctances to index
+        user_id = self.user_id.replace("/", "slash").replace("-", "hyphen")
         logger.debug("inside add_custom_entities ---")
         if self.conn is None:
             logger.debug("inside add_custom_entities --- inside if")
@@ -41,21 +42,21 @@ class Index:
         for entity_substr, entity_id, tag in zip(entity_substr_list, entity_ids_list, tags_list):
             logger.debug("inside add_custom_entities --- inside for")
             entity_id = entity_id.replace("/", "slash").replace("-", "hyphen")
-            query_str = f"title:{entity_substr} AND tag:{tag} AND user_id:{self.user_id}"
+            query_str = f"title:{entity_substr} AND tag:{tag} AND user_id:{user_id}"
 
             query = "SELECT * FROM inverted_index WHERE inverted_index MATCH ?;"
             res = self.cur.execute(query, (query_str,)).fetchall()
             logger.debug(f"the result is {res}")
             if res and res[0][3] == "name" and res[0][1] == entity_id and tag == "name":
                 query = "DELETE FROM inverted_index WHERE entity_id=? AND tag=? AND user_id=?;"
-                self.cur.execute(query, (entity_id, tag, self.user_id))
+                self.cur.execute(query, (entity_id, tag, user_id))
                 self.cur.execute("INSERT INTO inverted_index "
-                                 "VALUES (?, ?, ?, ?, ?);", (entity_substr.lower(), entity_id, 1, tag, self.user_id))
+                                 "VALUES (?, ?, ?, ?, ?);", (entity_substr.lower(), entity_id, 1, tag, user_id))
                 self.conn.commit()
                 logger.debug("deleted and posted")
             elif not res:
                 self.cur.execute("INSERT INTO inverted_index "
-                                 "VALUES (?, ?, ?, ?, ?);", (entity_substr.lower(), entity_id, 1, tag, self.user_id))
+                                 "VALUES (?, ?, ?, ?, ?);", (entity_substr.lower(), entity_id, 1, tag, user_id))
                 self.conn.commit()
                 logger.debug("Just posted")
 
